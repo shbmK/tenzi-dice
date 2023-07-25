12b3dc904e4590cef96bc7c8fc97319582a8467f
shbmK <shubham.kirtinath@yahoo.in>
2023-07-26 03:02:13 +0530

    implementation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Die from './components/Die'
 import './App.css'
 
 function App() {
   const [dieEl,setDieEl]=useState(diceElem())
+  const [tenzies,setTenzies]=useState(false)
   function diceElem(){
     const ar=[]
+    let id=1
     for(let i=0;i<10;i++){
-      ar.push(Math.ceil(Math.random()*6))
+      ar.push({
+        value:Math.ceil(Math.random()*6),
+        held:false,
+        id:id
+      })
+      id++
     }
     return ar
   }
-  let dicearr=dieEl.map(val=><Die value={val}/>)
-  const rolldice=()=>setDieEl(diceElem())
+  function dieId(id){
+    setDieEl(prev=>prev.map(die=>{
+      return die.id==id?{...die,held:!die.held}:die
+    }))
+  }
+  useEffect(function(){
+    const held=dieEl.every(die=>die.held)
+    const firstval=dieEl[0].value
+    const eqval=dieEl.every(die=>firstval===die.value)
+    if (held&&eqval){
+      setTenzies(true)
+      alert("Won")
+    }
+  },[dieEl])
+  let dicearr=dieEl.map(val=><Die value={val.value} held={val.held} dieId={()=>dieId(val.id)}/>)
+  const rolldice=()=>setDieEl(prev=>prev.map(die=>{
+    return die.held?die:{...die,value:Math.ceil(Math.random()*6)}
+  }))
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.
+      </p>
       <div className="die-container">
         {dicearr} 
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,9 @@
 export default function Die(props) {
-  return (
-    <div className="die-face">
+    const styles={
+        backgroundColor:props.held?"#16db4b":"white"
+    }
+    return (
+    <div className="die-face" style={styles} onClick={props.dieId}>
         <h3 className="die-num">{props.value}</h3>
     </div>
   )
